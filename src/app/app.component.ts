053308f187a093f86c5b989b9a3fb42bc893a720
Uzair <s.uzairnoman@gmail.com>
2023-11-10 21:13:17 +0100

    feat(repo): basic api data check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RepoService } from './services/repo/repo.service';
+import { Subscription } from 'rxjs';
+import { RepositoryData } from './interfaces/repository-data';
+import { RepositoryListComponent } from './components/repository-list/repository-list.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RepositoryListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'circunomics-fe-challenge';
+export class AppComponent implements OnInit {
+
+  repos: RepositoryData[] = [];
+  pageNumber = 1;
+
+  constructor(
+    private RepoService: RepoService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getGitHubRepositories();
+  }
+
+  getGitHubRepositories(): void {
+    this.RepoService.getGitHubRepositories(this.pageNumber).subscribe((data) => {
+      this.repos = this.repos.concat(data.items);
+    });
+  }
+
 }
